3ea1b9e3f6bdf41f8a42063ae4445434c317b009
EthanZGQ <799603679@qq.com>
2023-03-03 19:03:04 +0800

    before build the line

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,12 @@ int main(){
     std::cout << "Hello World!" << std::endl;
     TGAImage img(256,256,TGAImage::RGB);
     TGAColor color(255,0,255,255);
-    img.set(128,128,color);
+    for(int row = 0 ; row < 256 ;++row){
+        for(int col = 0 ; col < 256 ; ++col){
+            img.set(row,col,color);
+        }
+    }
+    img.set(0,0,color);
     img.write_tga_file("first.tga");
     std::cout << "convert complete!"<<std::endl;
     return 0;
